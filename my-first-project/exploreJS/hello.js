1381609c277626f311d4f1c17e66654efddf0202
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-12 01:01:54 +0600

    some js exercise like variable, convert, typeof, datatype

--- my-first-project/exploreJS/hello.js
+++ my-first-project/exploreJS/hello.js
@@ -1,1 +1,12 @@
-console.log(47);
+// console.log('Hello JS Dude');
+
+// var isMotherInHouse = false;
+
+// console.log(isMotherInHouse);
+// console.log(typeof isMotherInHouse);
+
+// var number = 15;
+// var number2 = '' + 25.5;
+
+// console.log(number + number2);
+// console.log(typeof (number + number2));
