91363e51e72cd7986d88ea35ab9e1ea29a34c449
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-12 23:15:30 +0600

    array exampe

---
+++ my-first-project/exploreJS/array.js
@@ -0,0 +1,16 @@
+var friendAge = [12,13,14,15]
+
+// friendAge.push(16);
+// console.log(friendAge.length);
+
+// friendAge.pop();
+// console.log(friendAge.length);
+
+// friendAge.shift();
+
+// friendAge.unshift(10);
+
+// console.log(friendAge.shift());
+
+console.log(friendAge.slice(2));
+console.log(friendAge.slice(1,3));
