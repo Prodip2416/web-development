4560af3f10bf62846e56ff85f0a1162891cbbf2c
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-12 10:53:20 +0600

    Math absolute round floor ceil random

--- my-first-project/exploreJS/hello.js
+++ my-first-project/exploreJS/hello.js
@@ -10,3 +10,10 @@
 
 // console.log(number + number2);
 // console.log(typeof (number + number2));
+
+var number = 12.0001;
+
+console.log(Math.round(number));
+console.log(Math.random(number));
+console.log(Math.floor(number));
+console.log(Math.ceil(number));
