75d9a3132c9693320c659bc4fb62464c909d4cee
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-14 00:40:47 +0600

    fibonacchi recursive

--- my-first-project/exploreJS/fibonacchi.js
+++ my-first-project/exploreJS/fibonacchi.js
@@ -6,10 +6,25 @@
 
 // console.log(fibo);
 
-function fibo(n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return fibo(n - 1) + fibo(n - 2)
+// function fibo(n) {
+//     if (n == 0) return 0;
+//     if (n == 1) return 1;
+//     return fibo(n - 1) + fibo(n - 2)
+// }
+
+// console.log(fibo(10));
+
+
+function fibonacchi(n) 
+{
+    if (n == 0) return [0]
+    if (n == 1) return [0,1]
+    else{
+        var fibo = fibonacchi(n - 1);
+        var nextElement = fibo[n-1] + fibo[n-2];
+        fibo.push(nextElement);
+        return fibo;
+    }
 }
 
-console.log(fibo(10));
+console.log(fibonacchi(1));
