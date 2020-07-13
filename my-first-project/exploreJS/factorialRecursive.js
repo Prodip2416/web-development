689aa2f59c123e4e4b24057f51749691664affd1
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-13 23:55:54 +0600

    explaniation of factorial

--- my-first-project/exploreJS/factorialRecursive.js
+++ my-first-project/exploreJS/factorialRecursive.js
@@ -1,3 +1,12 @@
+
+// 2! = 1*2
+// 3! = 1*2*3
+// 4! = 1*2*3*4 
+// 5! = 4! * 5
+// 6! = (6-1)!* 6
+// n! = (n-1)! * n
+
+
 function factorial(n){
     if(n == 0) return 1;
     return n * factorial(n - 1);
