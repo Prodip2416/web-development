cbb9d2ca75be085f0e845d4dd5602d7e82466ca6
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-14 23:40:23 +0600

    find max min

---
+++ my-first-project/exploreJS/findMaxMin.js
@@ -0,0 +1,6 @@
+var num1 = 350, num2 = 370, num3 = 560, num4 = 450, num5 = 150, num6 = 980;
+var resultMax = Math.max(num1, num2, num3, num4, num5, num6);
+var resultMin = Math.min(num1, num2, num3, num4, num5, num6);
+
+console.log(resultMax);
+console.log(resultMin);
