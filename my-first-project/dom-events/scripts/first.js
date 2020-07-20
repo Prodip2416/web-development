c4ef6fa3842a3fd959133c1abb624efa81377fa8
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-20 22:43:30 +0600

    getElement exercise

--- my-first-project/dom-events/scripts/first.js
+++ my-first-project/dom-events/scripts/first.js
@@ -1,2 +1,12 @@
-console.log(19);
-debugger;
+
+var articles = document.getElementsByClassName('article');
+console.log(articles);
+
+for(var i =0; i<articles.length;i++){
+    console.log(articles[i].innerText);
+    articles[i].style.backgroundColor='red';
+}
+
+document.getElementById('heading').style.color='white';
+
+document.getElementById('heading').style.fontSize='50px';
