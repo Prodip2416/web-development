c212eca21345ae32fc27b1b2546203a2953a56e6
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-21 09:27:59 +0600

    add event handler directly on an element

--- my-first-project/js-faq/arguments.js
+++ my-first-project/js-faq/arguments.js
@@ -4,3 +4,5 @@ function addNumber(firstNumber, secondNumber){
 }
 
 addNumber(2,4,5,6,7);
+var result = addNumber(2,4);
+console.log(result);
---
+++ my-first-project/js-faq/events.js
@@ -0,0 +1,15 @@
+function clickHandler() {
+    console.log('Someone Click Me.');
+}
+
+function doubleClickHandler() {
+    console.log('Someone Double Click Me.');
+}
+
+function onMouseWheelHandler() {
+    console.log('Someone on Mouse Wheel.');
+}
+
+function onMouseOutHandler() {
+    console.log('Someone on Mouse Uut.');
+}
