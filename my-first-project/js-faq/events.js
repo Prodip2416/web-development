23d89025392d2eb823af449bb7948dd05be3a2c6
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-21 09:53:23 +0600

    add event listener using javascript

--- my-first-project/js-faq/events.js
+++ my-first-project/js-faq/events.js
@@ -6,10 +6,20 @@ function doubleClickHandler() {
     console.log('Someone Double Click Me.');
 }
 
-function onMouseWheelHandler() {
-    console.log('Someone on Mouse Wheel.');
+function onMouseMoveHandler() {
+    console.log('Someone on Mouse Move.');
+    document.body.style.backgroundColor = 'gray'
 }
 
 function onMouseOutHandler() {
     console.log('Someone on Mouse Uut.');
 }
+
+const thirdButton = document.getElementById('btn-third');
+thirdButton.onmousemove = onMouseMoveHandler;
+
+// Add Event Listener Example
+document.getElementById('btnListener').addEventListener('click', clickHandler);
+document.getElementById('btnListener').addEventListener('click', function(){
+    document.body.style.color='tomato';
+});
