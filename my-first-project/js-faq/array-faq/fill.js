0071cc4e7e780bf7f01ac4dae95aeb347d2739de
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-29 22:42:56 +0600

    fill at array

---
+++ my-first-project/js-faq/array-faq/fill.js
@@ -0,0 +1,14 @@
+var my_array = [1,2,3,4,5];
+
+// my_array.fill(76);
+// console.log(my_array);
+
+// my_array.fill(76,0,3);
+// console.log(my_array);
+
+my_array.fill(76,1,3);
+console.log(my_array);
+
+var arr = [1, 23, 46, 58];
+arr.fill(87, 1, 3); 
+console.log(arr);
