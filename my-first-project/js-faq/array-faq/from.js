bfcacea082a78a39ec7f23732ab560d3f7e3417c
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-29 23:48:40 +0600

    from in array

---
+++ my-first-project/js-faq/array-faq/from.js
@@ -0,0 +1,11 @@
+var arr = "geeksforgeeks";
+
+console.log(Array.from(arr));
+
+// result
+// [
+//     'g', 'e', 'e', 'k',
+//     's', 'f', 'o', 'r',
+//     'g', 'e', 'e', 'k',
+//     's'
+//   ]
