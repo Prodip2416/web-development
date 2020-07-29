779b185e854209ee165daf9fa80b771cab2f81e7
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-29 09:58:28 +0600

    of an array

---
+++ my-first-project/js-faq/array-faq/of.js
@@ -0,0 +1,7 @@
+console.log(Array.of(0, 0, 0)); 
+console.log(Array.of(11, 21, 33)); 
+console.log(Array.of("Ram","Geeta")); 
+console.log(Array.of('Prodip')); 
+console.log(Array.of(2,3,4,'Sheeta')); 
+
+console.log(Array.of(['Ram', 'Rahim', 'Geeta', 'Sheeta'])); 
