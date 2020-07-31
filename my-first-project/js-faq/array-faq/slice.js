a8aae5bd549266348402ca043ef0a769e954ba90
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-31 23:43:54 +0600

    slice in array

---
+++ my-first-project/js-faq/array-faq/slice.js
@@ -0,0 +1,10 @@
+var my_array = [1,2,3,4,5,6];
+
+console.log(my_array.slice(2));
+console.log(my_array.slice(2,4));
+console.log(my_array.slice(3,5));
+
+// Result 
+// [ 3, 4, 5, 6 ]
+// [ 3, 4 ]
+// [ 4, 5 ]
