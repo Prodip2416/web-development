6498417dda57c460cc69b97c987e6e9498f3ac2d
prodip2416 <prodip.sarker.cse@gmail.com>
2020-07-31 22:10:16 +0600

    push of array

---
+++ my-first-project/js-faq/array-faq/push.js
@@ -0,0 +1,10 @@
+var my_array = [1,2,3];
+
+console.log(my_array);
+console.log(my_array.push(4,5,"Prodip",true,));
+console.log(my_array);
+
+// Result
+// [ 1, 2, 3 ]
+// 7
+// [ 1, 2, 3, 4, 5, 'Prodip', true ]
