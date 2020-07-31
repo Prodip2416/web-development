08d0c33daffa120cb9e30e93706d710a345e3f42
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-01 00:41:57 +0600

    trim in string

---
+++ my-first-project/js-faq/string-faq/trim.js
@@ -0,0 +1,11 @@
+var str = "          Hello        ";
+console.log(str);
+console.log(str.trimLeft());
+console.log(str.trimRight());
+console.log(str.trim());
+
+// Result 
+//           Hello
+// Hello
+//           Hello
+// Hello
