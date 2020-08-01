be74f9610d51ff3a6fc9679e3ab1f09d44e47dd0
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-01 11:57:37 +0600

    slice in strinr

---
+++ my-first-project/js-faq/string-faq/slice.js
@@ -0,0 +1,11 @@
+var str = 'Ram is going to school'; 
+
+console.log(str.slice(1));
+console.log(str.slice(2,5));
+console.log(str.slice(3));
+
+
+// Result
+// am is going to school
+// m i
+//  is going to school
