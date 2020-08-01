b003da90ce6db780ae8827762ff8b462d5a035fa
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-01 12:16:06 +0600

    indexOf  in string

---
+++ my-first-project/js-faq/string-faq/indexOF.js
@@ -0,0 +1,14 @@
+var str = 'It is a great day.';
+
+console.log(str.indexOf('great'));
+console.log(str.indexOf('day'));
+console.log(str.indexOf('a g'));
+console.log(str.indexOf(' i'));
+console.log(str.indexOf('.'));
+
+// Result
+// 8
+// 14
+// 6
+// 2
+// 17
