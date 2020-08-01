7670aa25cb57b332376d3ac5e6d471e74fe18b07
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-01 12:00:39 +0600

    sunStringe in strinr

---
+++ my-first-project/js-faq/string-faq/substring.js
@@ -0,0 +1,10 @@
+var str = 'It is a great day';
+
+console.log(str.substring(1,5));
+console.log(str.substring(5));
+console.log(str.substring(0));
+
+// Result
+// t is
+//  a great day
+// It is a great day
