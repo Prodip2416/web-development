7a73f9f86b6a786fe9b85063ab45be23ac49e60c
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-04 00:00:31 +0600

    isSaveInt in Number

---
+++ my-first-project/js-faq/number-faq/isSafeInteger.js
@@ -0,0 +1,11 @@
+console.log("Output : " + Number.isSafeInteger(0 / 0)); 
+console.log("Output : " + Number.isSafeInteger(0.5)); 
+console.log("Output : " + Number.isSafeInteger(-23)); 
+console.log("Output : " + Number.isSafeInteger(23)); 
+
+// Result
+
+// Output : false
+// Output : false
+// Output : true
+// Output : true
