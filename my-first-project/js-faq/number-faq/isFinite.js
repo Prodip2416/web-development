40153bfad233e32bd5a0fd6e6cd192113ed86fa6
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-03 23:56:32 +0600

    isFinite in Number

---
+++ my-first-project/js-faq/number-faq/isFinite.js
@@ -0,0 +1,14 @@
+console.log(Number.isFinite(12));
+console.log(Number.isFinite(12.24));
+console.log(Number.isFinite(-12));
+console.log(Number.isFinite(-12.24));
+console.log(Number.isFinite('12'));
+console.log(Number.isFinite("12"));
+
+// Result
+// true
+// true
+// true
+// true
+// false
+// false
