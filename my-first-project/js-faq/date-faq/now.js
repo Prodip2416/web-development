1aefff81285da849732712c7676330c672d7a208
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-05 19:57:40 +0600

    now in 'date

---
+++ my-first-project/js-faq/date-faq/now.js
@@ -0,0 +1,9 @@
+var todayDate = new Date();
+
+
+console.log(todayDate);
+console.log(todayDate.toString());
+
+// Result
+// 2020-08-05T13:57:06.249Z
+// Wed Aug 05 2020 19:57:06 GMT+0600 (Bangladesh Standard Time)
