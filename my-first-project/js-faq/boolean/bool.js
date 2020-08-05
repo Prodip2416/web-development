1220ea0a6cfe1f2279394467f46303c226090263
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-05 15:56:09 +0600

    boolean in bool

---
+++ my-first-project/js-faq/boolean/bool.js
@@ -0,0 +1,34 @@
+console.log('Boolean(10) is ' + Boolean(10)); 
+console.log('Boolean("GeeksforGeeks") is ' + Boolean("GeeksforGeeks")); 
+console.log('Boolean(2.74) is ' + Boolean(2.74)); 
+console.log('Boolean(-1) is ' + Boolean(-1)); 
+console.log("Boolean('true') is " + Boolean('true')); 
+console.log("Boolean('false') is " + Boolean('false')); 
+console.log('Boolean(3 * 2 + 1.11) is ' + Boolean(3 * 2 + 1.11)); 
+console.log('Boolean(1<2) is ' + Boolean(1 < 2)); 
+
+// Result
+// Boolean(10) is true
+// Boolean("GeeksforGeeks") is true
+// Boolean(2.74) is true
+// Boolean(-1) is true
+// Boolean('true') is true
+// Boolean('false') is true
+// Boolean(3 * 2 + 1.11) is true
+// Boolean(1<2) is true
+
+
+console.log('Boolean(0) is ' + Boolean(0)); 
+console.log('Boolean(-0) is ' + Boolean(-0)); 
+console.log('Boolean(false) is ' + Boolean(false)); 
+console.log('Boolean(NaN) is ' + Boolean(NaN)); 
+console.log('Boolean(null) is ' + Boolean(null)); 
+console.log('Boolean(1>2) is ' + Boolean(1 > 2)); 
+
+// Result
+// Boolean(0) is false
+// Boolean(-0) is false
+// Boolean(false) is false
+// Boolean(NaN) is false
+// Boolean(null) is false
+// Boolean(1>2) is false
